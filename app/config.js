02b191f62b643e3235ebb6ebc524d63e3df8bc23
Klaus-Peter Junghanns <kapejod@gmail.com>
2015-04-16 14:56:27 +0200

    Remove config.js changes.

--- app/config.js
+++ app/config.js
@@ -3,16 +3,7 @@ var config_module = angular.module('ahoyApp.config', []);
 var config_data = {
   AHOY_CONFIG: {
     title: 'Ahoy! Conference',
-    allow_dynamic_conferences: true,
-    wsUrl: "wss://director.ahoyconference.com",
-    iceServers: [
-    	{
-    	    url: "turn:dev.ahoyrtc.com:443?transport=tcp",
-    	    urls: "turn:dev.ahoyrtc.com:443?transport=tcp",
-    	    username: "turn",
-    	    credential: "pass"
-    	}
-    ]
+    allow_dynamic_conferences: true
   }
 };
 
