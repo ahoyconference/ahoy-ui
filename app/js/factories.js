75eee88ec94c6174761ddb6f121b1becb7b1b395
Klaus-Peter Junghanns <kapejod@gmail.com>
2015-04-23 14:14:30 +0200

    Use full URL as smartbanner argument.

--- app/js/factories.js
+++ app/js/factories.js
@@ -4,7 +4,7 @@ angular.module('ahoyApp.factories', [])
     .factory("smartBanner", function() {
 	var argument = "";
 	if (document.location.hash.indexOf("#/link/") != -1) {
-	    argument = document.location.hash.substring("#/link/".length);;
+	    argument = "ahoyconference://link/"+document.location.hash.substring("#/link/".length);;
 	}
 	return {
     	    appId: "973996885",
